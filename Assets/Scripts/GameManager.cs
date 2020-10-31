e2a899a055b74c0be5b7a77a694edd63d630c90b
danieljt <danieljamestarplett@hotmail.com>
2020-10-31 10:33:13 +0100

    Added simple game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Simple manager that just quits the program when escape is pressed
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    private void Update()
+    {
+        bool exitGame = Input.GetButtonDown("Cancel");
+        if(exitGame)
+		{
+            Application.Quit(0);
+		}
+    }
+}
